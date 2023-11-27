502cb786df06b730961df0336563d022c23000d3
jacket0 <MetaM111@yandex.com>
2023-11-27 22:31:50 +0300

    меню и класс Utils
    главное меню и класс Utils с функциями получение числа от пользователя и случайных чисел

--- Shaker/Shaker/Program.cs
+++ Shaker/Shaker/Program.cs
@@ -10,7 +10,34 @@ namespace Shaker
     {
         static void Main(string[] args)
         {
+            const int ConsoleFillCommand = 1;
+            const int FileFillCommand = 2;
 
+
+            List<int> numbers = new List<int>();
+            int userInput = 0;
+
+            do
+            {
+                Console.WriteLine("Выберите способ заполнения масисва чисел:" +
+                    $"\n [{ConsoleFillCommand}] Заполнение случайными числами от -100 до 100" +
+                    $"\n [{FileFillCommand}] Заполнение из файла");
+                userInput = Utils.GetNumber();
+
+                switch (userInput)
+                {
+                    case ConsoleFillCommand:
+                        //
+                        break;
+                    case FileFillCommand:
+                        //
+                        break;
+                    default:
+                        Console.WriteLine("Неверный пункт меню!");
+                        break;
+                }
+            }
+            while (userInput != ConsoleFillCommand && userInput != FileFillCommand);
         }
     }
 }
---
+++ Shaker/Shaker/Utils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shaker
+{
+    internal class Utils
+    {
+        private static Random s_random = new Random();
+
+        public static int GenerateRandomNumber(int minNumber, int maxNumber)
+        {
+            return s_random.Next(minNumber, maxNumber);
+        }
+
+        public static int GenerateRandomNumber(int maxNumber)
+        {
+            return s_random.Next(maxNumber);
+        }
+
+        public static int GetNumber()
+        {
+            int number;
+            string word = Console.ReadLine();
+
+            while (int.TryParse(word, out number) == false)
+            {
+                Console.WriteLine("Ошибка, введите число:  ");
+                word = Console.ReadLine();
+            }
+
+            return number;
+        }
+    }
+
+}
