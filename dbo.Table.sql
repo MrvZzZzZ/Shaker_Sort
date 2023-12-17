8cc8a61d2067824fb8e1f76a60cd819ebb61bec4
jacket <metam111@yandex.ru>
2023-12-17 22:00:33 +0300

    я устал

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT          NOT NULL,
+	[size_list] int		 null,
+	[sort_status] binary NOT null,
+	[list] varchar(MAX)    null,
+	PRIMARY KEY CLUSTERED ([Id] ASC)
+)
